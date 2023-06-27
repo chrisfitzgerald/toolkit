8f66cc2c114eaf2b1d63277ba480e0211c63a0f7
Chris Fitzgerald <cfitzgerald8888@gmail.com>
2023-06-26 22:11:17 -0500

    add file for PopulateCompleteionAge

---
+++ PopulateCompletionAge_Converter.ps1
@@ -0,0 +1,13 @@
+#FULLTEXTCATALOGPROPERTY PopulateCompletionAge	
+#The difference in seconds between the completion of the last full-text index population and 01/01/1990 00:00:00.
+
+##Uncomment the following line if you recieve the error '.ps1 is not digitall signed' 
+##Set-ExecutionPolicy -Scope Process -ExecutionPolicy Bypass
+
+$PopulateCompletionAge = 234234 ##enter seconds value from PopulateCompleteionAge column
+
+$referenceTime = Get-Date -Year 1990 -Month 1 -Day 1 -Hour 0 -Minute 0 -Second 0
+$timeDifference = New-TimeSpan -Seconds $PopulateCompletionAge
+$resultTime = $referenceTime + $timeDifference
+
+Write-Output "Result: $resultTime"
